7dce6fa48760d9430d707ab1d9e34220e0db3fb1
Cesar19170159 <19170159@uttcampus.edu.mx>
2021-07-16 14:37:46 -0500

    Actualizacion Index

--- login/db-connection.php
+++ login/db-connection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'admin');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'proyectoestadias-implan');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
